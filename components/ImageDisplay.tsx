d0963d684c913db0945dee9c2d78a5e821d800a0
Arctenox <69485661+AstrionX@users.noreply.github.com>
2025-09-09 05:46:11 -0400

    feat(ImageDisplay): Add upscale button
    Adds a new button to the image display component that allows users to upscale generated images, enhancing their quality and detail.

--- components/ImageDisplay.tsx
+++ components/ImageDisplay.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import LoadingSpinner from './LoadingSpinner';
 import { GenerationHistoryEntry, ImageConfig } from '../types';
@@ -161,6 +162,9 @@ const ImageDisplay: React.FC<ImageDisplayProps> = ({
                       </ImageActionButton>
                       <ImageActionButton onClick={() => onFixFace(entry.id, entry.imageDataUrl)} disabled={isAnyActionRunning} tooltip="Fix Facial Artifacts">
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" /></svg>
+                      </ImageActionButton>
+                      <ImageActionButton onClick={() => onUpscale(entry.id, entry.imageDataUrl)} disabled={isAnyActionRunning} tooltip="Upscale & Enhance Detail">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /></svg>
                       </ImageActionButton>
                        <ImageActionButton onClick={() => onMagicEdit(entry.id, entry.imageDataUrl)} disabled={isAnyActionRunning} tooltip="Magic Edit (Cloud)">
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
